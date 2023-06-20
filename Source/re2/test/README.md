4c57a025a5b2dd6179c3cc5fed15f59e6186feed
thejinchao <thejinchao@gmail.com>
2023-06-20 21:25:31 +0800

    add test program for re2(win64, mac and ps5)

---
+++ Source/re2/test/README.md
@@ -0,0 +1,12 @@
+## 1. Windows
+```
+cmake -G "Visual Studio 16 2019" %TL_LIBRARIES_PATH%/Source/re2/test
+```
+## 2. Mac
+```
+cmake -G "Unix Makefiles" $TL_LIBRARIES_PATH/Source/re2/test
+```
+## 3. PS5
+```
+"%SCE_ROOT_DIR%\Prospero\Tools\CMake\PS5CMake.bat" -G "Visual Studio 16 2019" %TL_LIBRARIES_PATH%/Source/re2/test
+```
---
+++ Source/re2/test/main.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+#include <assert.h>
+
+#include <re2/re2.h>
+
+int main(int argc, char* argv[]) 
+{
+	int number;
+	std::string str;
+	assert(RE2::FullMatch("test:1234", "(\\w+):(\\d+)", &str, &number));
+	printf("string=%s, number=%d\n", str.c_str(), number);
+	assert(str == "test");
+	assert(number == 1234);
+
+	return 0;
+}
